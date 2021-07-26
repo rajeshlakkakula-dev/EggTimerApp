18a0ca52b56472b79dc56203d5ed5a7e3814b4a8
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2021-07-26 18:24:28 +0530

    This commit contains UI design of Egg Timer App

---
+++ app/src/main/java/com/rl/eggtimerapp/ImportantActivity.kt
@@ -0,0 +1,13 @@
+package com.rl.eggtimerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImportantActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_important)
+    }
+}
